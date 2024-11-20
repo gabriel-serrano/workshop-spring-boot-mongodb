7368a1ee034fd11a3827250f857c021730935447
Gabriel Serrano <gabriel.serrano@outlook.com.br>
2024-11-20 10:59:45 -0300

    Acrescentando comentários aos posts

--- src/main/java/br/com/gabrielserrano/workshopmongo/config/Instantiation.java
+++ src/main/java/br/com/gabrielserrano/workshopmongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package br.com.gabrielserrano.workshopmongo.config;
 import br.com.gabrielserrano.workshopmongo.domain.Post;
 import br.com.gabrielserrano.workshopmongo.domain.User;
 import br.com.gabrielserrano.workshopmongo.dto.AuthorDTO;
+import br.com.gabrielserrano.workshopmongo.dto.CommentDTO;
 import br.com.gabrielserrano.workshopmongo.repository.PostRepository;
 import br.com.gabrielserrano.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -51,6 +52,13 @@ public class Instantiation implements CommandLineRunner {
                 "Acordei feliz hoje!",
                 new AuthorDTO(maria)
         );
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1,c2));
+        post2.getComments().addAll(Arrays.asList(c3));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
